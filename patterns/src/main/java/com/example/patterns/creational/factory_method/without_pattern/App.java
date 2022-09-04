23bd265425a2e01cf2e7421cd7ab23576f2693e5
Kate Aksionava <kate.aks8488@gmail.com>
2022-09-04 23:50:51 +0300

    structural patterns

--- patterns/src/main/java/com/example/patterns/creational/factory_method/without_pattern/App.java
+++ patterns/src/main/java/com/example/patterns/creational/factory_method/without_pattern/App.java
@@ -6,13 +6,13 @@ import com.example.patterns.creational.factory_method.without_pattern.developers
 public class App {
 
     public static void main(String[] args) {
-        JavaDeveloper developer = new JavaDeveloper(); // need to change when work other developer
+        JavaDeveloper developer = new JavaDeveloper("d"); // need to change when work other developer
         developer.writeJavaCode();
-        KotlinDeveloper developer2 = new KotlinDeveloper();
+        KotlinDeveloper developer2 = new KotlinDeveloper("d");
         developer2.writeKotlinCode();
-        KotlinDeveloper developer3 = new KotlinDeveloper();
+        KotlinDeveloper developer3 = new KotlinDeveloper("d");
         developer3.writeKotlinCode();
-        KotlinDeveloper developer4 = new KotlinDeveloper();
+        KotlinDeveloper developer4 = new KotlinDeveloper("d");
         developer4.writeKotlinCode();
     }
 }
